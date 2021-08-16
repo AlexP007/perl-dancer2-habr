e94feeb24aba8e260dff94f8c1d1e668c9c920b5
Alexander Panteleev <aleksandr.panteleev@domrf.ru>
2021-08-16 22:05:14 +0300

    [i] part_II

--- lib/MyApp.pm
+++ lib/MyApp.pm
@@ -1,16 +1,24 @@
 package MyApp;
+
 use Dancer2;
+use Dancer2::Plugin::FlashNote;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
-    return "Let's dance!";
+    template 'index';
 };
+
+get '/logout' => sub {
+    flash 'Bye bye!';
+
+    redirect '/';
+}; 
 
 get '/pages/:page[Int]' => sub {
     my $page = route_parameters->get('page');
 
-    template 'page.tt', {
+    template 'page', {
         page => $page,
     };
 };
